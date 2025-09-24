8274f6654100c377c02be001e074ce6ad46110c3
UdayParkar <udayparkar2003@gmail.com>
2025-09-24 13:50:51 +0530

    updated color in app.js

--- bookstore-app/app/src/app.js
+++ bookstore-app/app/src/app.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
   const html = `
     <html><head><title>✈️ यात्रा अन्वेषक</title><style>
       body{font-family:Arial;margin:0;background:linear-gradient(to bottom,#e0c3fc,#fbc2eb);color:#333}
-      .header{background:#d63384;color:white;text-align:center;padding:40px 20px}
+      .header{background:#2ee8cc;color:white;text-align:center;padding:40px 20px}
       .header h1{margin:0;font-size:40px}
       .header p{margin:10px 0 0;font-size:18px}
       .packages{display:grid;grid-template-columns:repeat(auto-fit,minmax(320px,1fr));gap:20px;padding:30px}
